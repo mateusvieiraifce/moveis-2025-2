9e26324f9da66bac6d15adce9deb6eeb1fbc9083
Irisvane <sharon.bize2025@gmail.com>
2025-10-13 15:34:25 -0300

    subindo aula 4

---
+++ Aula04/navigate/src/app/index.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+        <Link href="/home">Go to Home</Link>
+        <Link href="/users">Go to User</Link>
+        <Link href="/products/123">Go to Produtos</Link>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Aula04/navigate/src/app/users/index.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>STela de Usuário</Text>
+      <Link href="/">Voltar para a Home</Link>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
